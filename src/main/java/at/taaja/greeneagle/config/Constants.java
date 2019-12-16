7c5fe3054a6e7bc77ed7754984fefb6a598da91e
kris <kris.raich@gmail.com>
2019-12-16 15:52:14 +0100

    added config and websocket

---
+++ src/main/java/at/taaja/greeneagle/config/Constants.java
@@ -0,0 +1,8 @@
+package at.taaja.greeneagle.config;
+
+public class Constants {
+
+    //public static final String TOPIC_VEHICLE_UPDATE = "vehicleUpdate";
+    public static final String TOPIC_VEHICLE_UPDATE = "vehicle";
+
+}
