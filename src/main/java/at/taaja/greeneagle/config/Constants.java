b84b0aa62d226ea4fdf2abab02c5ed8165aa451b
kris <kris.raich@gmail.com>
2020-01-22 15:38:12 +0100

    removed swing and added quarkus

--- src/main/java/at/taaja/greeneagle/config/Constants.java
+++
@@ -1,8 +0,0 @@
-package at.taaja.greeneagle.config;
-
-public class Constants {
-
-    //public static final String TOPIC_VEHICLE_UPDATE = "vehicleUpdate";
-    public static final String TOPIC_VEHICLE_UPDATE = "posUpdate";
-
-}
---
+++ src/test/java/at/taaja/greeneagle/NativePositionResourceIT.java
@@ -0,0 +1,9 @@
+package at.taaja.greeneagle;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativePositionResourceIT extends PositionResourceTest {
+
+    // Execute the same tests but in native mode.
+}
