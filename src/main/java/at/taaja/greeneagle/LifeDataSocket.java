a1d064cede60cca56b4b359d013e4aa87c9f517c
kris <kris.raich@gmail.com>
2020-03-30 16:48:38 +0200

    added docu

--- src/main/java/at/taaja/greeneagle/LifeDataSocket.java
+++ src/main/java/at/taaja/greeneagle/LifeDataSocket.java
@@ -5,6 +5,7 @@ import org.eclipse.microprofile.config.inject.ConfigProperty;
 import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.server.ServerEndpoint;
 
+//url: ws://localhost:8091/v1/data/c56b3543-6853-4d86-a7bc-1cde673a5582
 @ServerEndpoint("/v1/data/{id}")
 @ApplicationScoped
 public class LifeDataSocket extends AbstractSocket{
