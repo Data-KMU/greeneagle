eb4d42de3d68dec301b8b962e8c4e169ef41dbdd
kris <kris.raich@gmail.com>
2020-10-28 19:56:35 +0100

    update version

--- src/main/java/at/taaja/greeneagle/KafkaDataService.java
+++ src/main/java/at/taaja/greeneagle/KafkaDataService.java
@@ -1,47 +1,34 @@
 package at.taaja.greeneagle;
 
-import io.quarkus.runtime.ShutdownEvent;
-import io.quarkus.runtime.StartupEvent;
+import io.reactivex.Emitter;
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.subscription.MultiEmitter;
+import io.smallrye.mutiny.subscription.MultiSubscriber;
+import io.taaja.services.AbstractKafkaConsumerService;
 import lombok.extern.jbosslog.JBossLog;
-import org.apache.kafka.clients.consumer.*;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
 import javax.websocket.Session;
-import java.time.Duration;
+import java.util.LinkedList;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Queue;
-import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 @ApplicationScoped
 @JBossLog
-public class KafkaDataService {
-
-
-    @ConfigProperty(name = "kafka.bootstrap-servers")
-    private String bootstrapServers;
-
-    @ConfigProperty(name = "kafka.poll-records")
-    private int pollRecords;
-
-    @ConfigProperty(name = "kafka.auto-commit")
-    private boolean autoCommit;
-
-    @ConfigProperty(name = "kafka.offset-reset")
-    private String offsetReset;
+public class KafkaDataService extends AbstractKafkaConsumerService {
 
 //     topic -> Queue of Sessions
     private Map<String, Queue<Session>> connections = new ConcurrentHashMap<>();
-    private Consumer<String, String> kafkaConsumer;
-    private boolean isRunning = false;
-    private AtomicBoolean subscriberChanged = new AtomicBoolean(false);
-    private Thread puller;
+
 
     public void registerClient(Session session, String topic){
         //is topic already subscribed
@@ -51,11 +38,7 @@ public class KafkaDataService {
             Queue<Session> sessions = new ConcurrentLinkedQueue();
             sessions.add(session);
             this.connections.put(topic,sessions);
-            this.updateConsumerList();
-            if(!isRunning){
-                isRunning = true;
-                this.startPolling();
-            }
+
         }
     }
 
@@ -68,63 +51,38 @@ public class KafkaDataService {
         sessions.remove(session);
 
         if(sessions.isEmpty()){
-            //unsubscribe
             this.connections.remove(topic);
-            this.updateConsumerList();
         }
     }
 
-    private void updateConsumerList(){
-        this.subscriberChanged.set(true);
-    }
 
 
-    //init kafka
-    void onStart(@Observes StartupEvent ev) {
-        final Properties consumerProperties = new Properties();
-        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
-        consumerProperties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, this.pollRecords);
-        consumerProperties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, this.autoCommit);
-        consumerProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, this.offsetReset);
+    @Override
+    protected void processRecord(final ConsumerRecord<String, String> consumerRecord) {
 
-        String uuid = UUID.randomUUID().toString();
-        consumerProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "greeneagle-" + uuid);
-        consumerProperties.put(ConsumerConfig.CLIENT_ID_CONFIG, "client-" + uuid);
-        this.kafkaConsumer = new KafkaConsumer(consumerProperties, new StringDeserializer(), new StringDeserializer());
-        log.info("kafka created");
+        Queue<Session> sessions = this.connections.get(consumerRecord.topic());
 
-    }
-
-    private void startPolling(){
-        this.puller = new Thread(() -> {
-            while(isRunning) {
-                if(this.subscriberChanged.getAndSet(false)){
-                    this.kafkaConsumer.subscribe(this.connections.keySet());
+        if(sessions != null){
+            sessions.forEach(s -> s.getAsyncRemote().sendObject(consumerRecord.value(), result ->  {
+                Throwable exception = result.getException();
+                if (exception != null) {
+                    log.warn("Unable to send message: " + exception.getMessage(), exception);
                 }
-                ConsumerRecords<String, String> records = this.kafkaConsumer.poll(Duration.ofMillis(100));
-                for(ConsumerRecord<String, String> record : records){
-                    Queue<Session> sessions = this.connections.get(record.topic());
-                    if(sessions == null) {
-                        log.error("received unknown topic: " + record.topic());
-                    }else{
-                        sessions.forEach(s -> s.getAsyncRemote().sendObject(record.value(), result ->  {
-                            Throwable exception = result.getException();
-                            if (exception != null) {
-                                log.warn("Unable to send message: " + exception.getMessage(), exception);
-                            }
-                        }));
-                    }
-                }
-            }
-        });
-        this.puller.setName("poll thread");
-        this.puller.start();
+            }));
+
+        }
+
     }
 
-    void onStop(@Observes ShutdownEvent ev) {
-        log.info("shutdown kafka");
-        this.isRunning = false;
-        this.kafkaConsumer.close();
-    }
+//todo
+
+//    public Multi<String> sseStreamForId(final String extensionId) {
+//        return Multi.createFrom().items(new Supplier<Stream<? extends String>>() {
+//            @Override
+//            public Stream<? extends String> get() {
+//                return null;
+//            }
+//        }) items(blockingQueue.stream());
+//    }
 
 }
--- src/main/java/at/taaja/greeneagle/LifeData.java
+++ src/main/java/at/taaja/greeneagle/LifeData.java
@@ -4,14 +4,29 @@ import io.taaja.kafka.Topics;
 import lombok.extern.jbosslog.JBossLog;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.websocket.*;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
+
+
 @ServerEndpoint("/v1/extension/{id}")
+//@Path("/v1/")
 @ApplicationScoped
 @JBossLog
 public class LifeData extends AbstractSocket{
+
+
+//    @GET
+//    @Produces(MediaType.SERVER_SENT_EVENTS)
+//    @SseElementType(MediaType.TEXT_PLAIN)
+//    @Path("/extension/{id}")
+//    public Multi<String> greetingsAsStream(@PathParam("id") String extensionId) {
+//        return this.kafkaDataService.sseStreamForId(this.getTopic(extensionId));
+//    }
 
     @OnOpen
     public void onOpen(Session session, @PathParam("id") String id) {
